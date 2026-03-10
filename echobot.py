83e397c8af6962b8808aa9ba7b8af9f25643a104
ZeroPancakes <dominoplayer79@gmail.com>
2026-03-10 16:30:40 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("DominicWhisenant_EchoBot") 
 
-with st.chat_message("assistant"):
-  st.image("TheSavedImage.png", caption="CIT 144 – Demographics Data Visualization")
+with st.chat_message("assistant"): 
+  st.image("TheSavedImage.png", caption="CIT 144 - Demographics Data Visualization")
 
 #Initialize chat history 
 if "messages" not in st.session_state: 
